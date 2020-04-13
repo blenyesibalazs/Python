c55e8b27974eb3ed51389a5aafb7c84b7ad46b85
bblenyesi <balazs.blenyesi@itiviti.com>
2020-04-13 13:49:47 +0300

    first tutorial

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,54 @@
-print("Hello world!")
+#print("Hello world!")
+#
+#the_world_is_flat = True
+#if the_world_is_flat : 
+#    print("Be Careful not to fall off!")
+#
+# this is the way to comment! 
+#int_variable = 1
+#print(int_variable)
+
+#print(2/3) # returns a float
+#print(2//3) # returns an integer, discards fractional results
+#print(2%3) # returns remmainder
+#print(2**7) # ** means raising to the power of
+
+#print(r"C:\name") # r means raw strings and foregoes the new line ES. if not use \ for esape sequence
+#print("C:\name")
+
+# print(3*'tacsko' + 'szep') #strings can be multiplied with * and concatenated by +
+#
+#text = ("this is the life"    # automatic concat
+#            "fuck the police")
+#print(text)
+#
+#print(text[23]) # strings act as arrays, characters start at index 0
+#print(text[-1]) # negative means start from the righ
+#
+#print(text[0:4]) # slice and dice
+#print(text[:4])
+#print(text[:4]+text[4:]) #I can ommit
+
+# text[0]='A' # strings cannot be changed
+
+#s='retek'
+
+#print(len(s))
+
+#############lists
+
+#squares = [1,4,9,16,25]
+#
+#print(squares)
+#print(squares[0])
+#print(squares[:-2])
+# squares[2] = 'a'
+#print(squares)
+
+# nested lists
+#x = ['a', 'b' , 'c']
+#print(x)
+#y = ['A', 'B', 'C']
+#z = [x, y]
+#print(z)
+#print(z[0][1])

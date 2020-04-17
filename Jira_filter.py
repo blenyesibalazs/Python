5c9496a45af98ab4f58d4dc65cf41c0c3bd8479e
bblenyesi <balazs.blenyesi@itiviti.com>
2020-04-17 15:56:30 +0300

    filter results

--- Jira_filter.py
+++ Jira_filter.py
@@ -6,5 +6,7 @@ jira = Jira(
     password = 'Infront22222222!'
 )
 
-jql_request = 'project=FOBR and issuekey = FOBR-16143'
+jql_request = 'project = FOBR AND (assignee = amunlawin OR reporter = amunlawin) AND updatedDate >= startOfMonth(-1) AND updatedDate <= endOfMonth(-1)'
 ticket = jira.jql(jql_request)
+
+print(ticket)
